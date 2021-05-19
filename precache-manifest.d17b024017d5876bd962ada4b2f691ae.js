077495c89d2c63a77a50544fdf1c7a259d2fdd21
Rafael Araujo <rflaraujodev@gmail.com>
2021-05-19 20:12:05 -0300

    deploy

---
+++ precache-manifest.d17b024017d5876bd962ada4b2f691ae.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e83a07f038ba7f47432",
+    "url": "/flow/css/app.4991ab17.css"
+  },
+  {
+    "revision": "97d250e3cab676dbd28a",
+    "url": "/flow/css/auth.68d9523d.css"
+  },
+  {
+    "revision": "75af68fda89790d360a6",
+    "url": "/flow/css/chunk-vendors.40958154.css"
+  },
+  {
+    "revision": "78c66962f546d09753af",
+    "url": "/flow/css/qrcode.10b84f64.css"
+  },
+  {
+    "revision": "a5ee087912cbedb6022426ea0b41f8bd",
+    "url": "/flow/fonts/Eva-Icons.a5ee0879.woff"
+  },
+  {
+    "revision": "cae252678f70ff0200acde68a6e74640",
+    "url": "/flow/fonts/Eva-Icons.cae25267.woff2"
+  },
+  {
+    "revision": "9c4b4d0d5ef2ead1e086fd1f71e5eb84",
+    "url": "/flow/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.9c4b4d0d.woff"
+  },
+  {
+    "revision": "df8803afe71155fa95130772bd0da593",
+    "url": "/flow/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.df8803af.woff2"
+  },
+  {
+    "revision": "147e3378b44bc9570418b1eece10dd7c",
+    "url": "/flow/fonts/materialdesignicons-webfont.147e3378.woff"
+  },
+  {
+    "revision": "7a44ea195f395e1d086010e44555a5c4",
+    "url": "/flow/fonts/materialdesignicons-webfont.7a44ea19.woff2"
+  },
+  {
+    "revision": "6921dd8a83eae034aab9292e9eb06f6a",
+    "url": "/flow/img/imagem1.6921dd8a.jpg"
+  },
+  {
+    "revision": "7a8dbd17d69414bdc63654f4717c1b1e",
+    "url": "/flow/img/imagem2.7a8dbd17.jpg"
+  },
+  {
+    "revision": "7ca2cc591d98c1b452c543d4389b3e78",
+    "url": "/flow/img/imagem3.7ca2cc59.jpg"
+  },
+  {
+    "revision": "c32be3c5e24e1e2b40d424869cb6b0e6",
+    "url": "/flow/img/imagem4.c32be3c5.jpg"
+  },
+  {
+    "revision": "1e49eec93248b9b48450807bb0435d1e",
+    "url": "/flow/img/imagem5.1e49eec9.jpg"
+  },
+  {
+    "revision": "2b3b90c32db0708290374f05222cb851",
+    "url": "/flow/img/imagem6.2b3b90c3.jpg"
+  },
+  {
+    "revision": "96947a9649b233571c293f4713120707",
+    "url": "/flow/img/imagem7.96947a96.jpg"
+  },
+  {
+    "revision": "7475195faa534884994060788d528dfa",
+    "url": "/flow/img/logo_flow.7475195f.svg"
+  },
+  {
+    "revision": "1c8a5c49df4ddd86e727bce07a83ec76",
+    "url": "/flow/index.html"
+  },
+  {
+    "revision": "c30a554a76bf4e30dcbe",
+    "url": "/flow/js/about.72d99c83.js"
+  },
+  {
+    "revision": "3e83a07f038ba7f47432",
+    "url": "/flow/js/app.249079c0.js"
+  },
+  {
+    "revision": "97d250e3cab676dbd28a",
+    "url": "/flow/js/auth.3c653a89.js"
+  },
+  {
+    "revision": "75af68fda89790d360a6",
+    "url": "/flow/js/chunk-vendors.389487e1.js"
+  },
+  {
+    "revision": "498ced18551742c5dbd7",
+    "url": "/flow/js/magazine.d3afe34b.js"
+  },
+  {
+    "revision": "78c66962f546d09753af",
+    "url": "/flow/js/qrcode.35c8fe02.js"
+  },
+  {
+    "revision": "e411b54d5a018f5db6dec1744c7b3b4e",
+    "url": "/flow/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/flow/robots.txt"
+  }
+]);
